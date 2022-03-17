062dd9a2709a903bd4464b598eaa42a18f0c658f
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-03-17 23:44:54 +0200

    Test Commit

--- Assets/#MyScripts/S_Quiz.cs
+++ Assets/#MyScripts/S_Quiz.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S_Quiz : MonoBehaviour
 {
     public Canvas _QuizCanvas; // Create Canvas Object to Initialize Quiz Canvas from the Scene
+    public Button[] AnsButtons;
+    public Button QuitButton;
+    public Button NextButton;
 
     // Start is called before the first frame update
     void Start()
